468f79d8047a9e33c95ad3efac9bc7a93ca7112c
SaiMukesh6 <madabattulamukesh@gmail.com>
2026-07-17 12:37:30 +0530

    Use modern langchain_text_splitters import

--- backend/rag.py
+++ backend/rag.py
@@ -3,7 +3,7 @@ import os
 from groq import Groq
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 
